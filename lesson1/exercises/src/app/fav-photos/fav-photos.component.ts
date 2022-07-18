e2c4ef9f16d0d023328b8ce1a5bb2393ffe8b341
Jordan <jordan_anthony11@aol.com>
2022-07-17 20:22:35 -0500

    Made changes to movie-list, chores-list, and fav-photos components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Photos!';
+  image1 = 'https://gray-kmov-prod.cdn.arcpublishing.com/resizer/99nnx9DYC_4yeGM-BAE7WP0KWdc=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/V4ID7BWXDRCYRAI73EK5PDJAVA.jpg';
+  image2 = 'https://www.gannett-cdn.com/presto/2020/03/17/USAT/c0eff9ec-e0e4-42db-b308-f748933229ee-XXX_ThinkstockPhotos-200460053-001.jpg?crop=1170%2C658%2Cx292%2Cy120&width=1200';
+  image3 = 'https://resize.indiatvnews.com/en/centered/newbucket/1200_675/2019/04/avengers-endgame-1556244382.jpg';
 
   constructor() { }
 
